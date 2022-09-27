b65a32e5af52eab12f12d1a1d903ee186a498e76
Alyssha Lewin <alyssha.lew@gmail.com>
2022-09-27 11:51:38 -0500

    added forwardAuthenticated middleware

---
+++ config/auth.js
@@ -0,0 +1,17 @@
+//Authentication Guard
+module.exports = {
+    //allows us to bring this file in an add as middleware to any route we want to be protected
+    ensureAuthenticated: function(req, res, next) {
+      if (req.isAuthenticated()) {
+        return next();
+      }
+      req.flash('error_msg', 'Please log in to view that resource');
+      res.redirect('/users/login');
+    },
+    forwardAuthenticated: function(req, res, next) {
+      if (!req.isAuthenticated()) {
+        return next();
+      }
+      res.redirect('/dashboard');      
+    }
+};
