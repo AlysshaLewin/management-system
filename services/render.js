de24e831208883f9aec9f47ac311bd23c9cdb34b
Alyssha <90856589+AlysshaLewin@users.noreply.github.com>
2022-09-28 18:01:15 -0500

    changed homeRoutes render to /dashboard

--- services/render.js
+++ services/render.js
@@ -5,7 +5,7 @@ exports.homeRoutes = (req, res) => {
     // Make a get request to /api/users
     axios.get('http://localhost:5000/api/users')
         .then(function(response){
-            res.render('index', { users : response.data });
+            res.render('dashboard', { users : response.data });
         })
         .catch(err =>{
             res.send(err);
