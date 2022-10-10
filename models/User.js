75f4be624ac8d530b436768e414dd771aee6a62d
Alyssha <90856589+AlysshaLewin@users.noreply.github.com>
2022-10-09 21:00:31 -0500

    updated User model schema

--- models/User.js
+++ models/User.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+const bcrypt = require("bcrypt");
 
 const UserSchema = new mongoose.Schema({
-    name: {
+    userName: {
         type: String,
         required: true
     },
@@ -19,6 +20,37 @@ const UserSchema = new mongoose.Schema({
     }
 });
 
-const User = mongoose.model('User', UserSchema);
+// Password hash middleware.
 
-module.exports = User;
+UserSchema.pre("save", function save(next) {
+    const user = this;
+    if (!user.isModified("password")) {
+      return next();
+    }
+    bcrypt.genSalt(10, (err, salt) => {
+      if (err) {
+        return next(err);
+      }
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        if (err) {
+          return next(err);
+        }
+        user.password = hash;
+        next();
+      });
+    });
+});
+  
+// Helper method for validating user's password.
+  
+UserSchema.methods.comparePassword = function comparePassword(
+    candidatePassword,
+    cb
+) {
+    bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+      cb(err, isMatch);
+    });
+};
+  
+module.exports = mongoose.model("User", UserSchema);
+  
