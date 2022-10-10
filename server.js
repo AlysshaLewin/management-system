238580c25512f5837640460be2974f5b9ca7158e
Alyssha <90856589+AlysshaLewin@users.noreply.github.com>
2022-10-09 20:55:54 -0500

    updated syntax

--- server.js
+++ server.js
@@ -1,68 +1,65 @@
-const express = require('express');
-const expressLayouts = require('express-ejs-Layouts');
-const mongoose = require('mongoose');
-const flash = require('connect-flash');
-const session = require('express-session');
-const connectDB = require('./config/database');
-const passport = require('passport');
-
+const express = require("express"); //helps build out API
 const app = express();
+const mongoose = require("mongoose"); //helps us talk to our MONGO DB
+const passport = require("passport"); //uses for authentication. Strats for logins
+const session = require("express-session"); //allows users to stay logged in across app
+const MongoStore = require("connect-mongo")(session); //stores our actual session in MONGO DB
+const methodOverride = require("method-override"); //allows us to use GET & POST throughout our entire app but treat them as PUT & DELETE as needed to do different things
+const flash = require("express-flash"); //shows all our notifications
+const logger = require("morgan"); //logs & shows everything happening in console
+const connectDB = require("./config/database"); //connects to our DB
+const mainRoutes = require("./routes/main"); //routes for homepage,login,signup
+const postRoutes = require("./routes/posts"); //routes for individual posts,feed,profile
 
-//Passport config
-require('./config/passport')(passport);
 
 //Use .env file in config folder
 require("dotenv").config({ path: "./config/.env" });
 
+// Passport config
+require("./config/passport")(passport);
+
 //Connect To Database
 connectDB();
 
-//EJS Middleware
-app.use(expressLayouts);
+//Using EJS for views
+app.set("view engine", "ejs"); //server now expects EJS files to be whats used to spit out our VIEWS
 
-// set view engine
-app.set('view engine', 'ejs');
+//Static Folder
+app.use(express.static("public")); //for any static assets(CSS,JS,IMGS) files
 
-//Bodyparser - included with Express
+//Body Parsing - allows us to pull stuff out of requests being made (like when submitting a form and pulling the data out of those forms)
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-//Express Session Middleware
+//Logging - using MORGAN to log requests in console
+app.use(logger("dev"));
+
+//Use forms for put / delete
+app.use(methodOverride("_method")); //allows to OVERRIDE any request that comes to server
+
+//Setup Sessions - stored in MongoDB - needed for session to be implimented
 app.use(
-    session({
-      secret: 'secret',
-      resave: true,
-      saveUninitialized: true
-    })
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
 );
 
-//Passport Middleware
+// Passport middleware - helps with authentication, allows user to stay logged in
 app.use(passport.initialize());
 app.use(passport.session());
 
-//Connect flash
+//Use flash messages for errors, info, ect...
 app.use(flash());
 
-//Global Variables
-app.use(function(req, res, next) {
-    res.locals.success_msg = req.flash('success_msg');
-    res.locals.error_msg = req.flash('error_msg');
-    res.locals.error = req.flash('error');
-    next();
-});
-
-
-//Static Folder
-app.use(express.static('assets'));
-
-//Routes - 
-//for index.js file in routes folder
-app.use('/', require('./routes/index'));
-app.use('/users', require('./routes/users')); 
-
+//Setup Routes For Which The Server Is Listening
+app.use("/", mainRoutes); //routes for homepage,login,signup
+app.use("/post", postRoutes); //routes for individual posts,feed,profile
 
 
 //Server Running
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on http://localhost:${process.env.PORT}/`);
-    // console.log("Server is running, you better catch it!");
+  console.log(`Server is running http://localhost:${process.env.PORT}/`);
 });
