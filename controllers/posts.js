227f56e38043cf9ef983fc60e957cb9d821b2a8c
Alyssha <90856589+AlysshaLewin@users.noreply.github.com>
2022-10-09 21:56:56 -0500

    added posts.js controllers

---
+++ controllers/posts.js
@@ -0,0 +1,101 @@
+const Post = require("../models/Post");
+const UserForm = require("../models/Form");
+
+
+module.exports = {
+  getProfile: async (req, res) => {
+    try {
+      const posts = await UserForm.find({ user: req.user.id });
+      res.render("profile.ejs", { posts: posts, user: req.user });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  getPost: async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+      res.render("post.ejs", { post: post, user: req.user });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  createPost: async (req, res) => {
+    try {
+      // Upload image to cloudinary
+      const result = await cloudinary.uploader.upload(req.file.path);
+
+      await Post.create({
+        title: req.body.title,
+        image: result.secure_url,
+        cloudinaryId: result.public_id,
+        caption: req.body.caption,
+        likes: 0,
+        user: req.user.id,
+      });
+      console.log("Post has been added!");
+      res.redirect("/profile");
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  likePost: async (req, res) => {
+    try {
+      await Post.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+          $inc: { likes: 1 },
+        }
+      );
+      console.log("Likes +1");
+      res.redirect(`/post/${req.params.id}`);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  deletePost: async (req, res) => {
+    try {
+      // Find post by id
+      let post = await Post.findById({ _id: req.params.id });
+      // Delete image from cloudinary
+      await cloudinary.uploader.destroy(post.cloudinaryId);
+      // Delete post from db
+      await Post.remove({ _id: req.params.id });
+      console.log("Deleted Post");
+      res.redirect("/profile");
+    } catch (err) {
+      res.redirect("/profile");
+    }
+  },
+  addUser: (req, res) => {
+    res.render('add_user');
+  },
+  updateUser: (req, res) =>{
+    if(req.query.id){
+      const id = req.query.id;
+
+      UserForm.findById(id)
+          .then(data =>{
+              if(!data){
+                  res.status(404).send({ message : "Not found user with id "+ id})
+              }else{
+                  // res.send(data)
+                  res.render("update_user", { user : data})
+                  //console.log(data, "test")
+              }
+          })
+          .catch(err =>{
+              res.status(500).send({ message: "Error retrieving user with id " + id})
+          })
+
+  }else{
+      UserForm.find()
+          .then(user => {
+              res.send(user)
+          })
+          .catch(err => {
+              res.status(500).send({ message : err.message || "Error occurred while retriving user information" })
+          })
+  }
+  },
+
+};
